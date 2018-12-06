3c8aff19cd0e3c2bf5bd1cb7481e445c5d11053c
dni <daniele@nicastro.io>
2018-12-06 21:22:19 +0100

    add with test unit

--- kata.py
+++ kata.py
@@ -1,6 +1,19 @@
+import unittest
+
+
 def max_number(n):
     sorted_number = sorted(list(str(n)), reverse=True)  # to string -> create a list -> sorting reverse
-    return ''.join(sorted_number)
+    return int(''.join(sorted_number))
 
 
-print(max_number(5825728))
+class TestStringMethods(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertEqual(max_number(12345), 54321)
+        self.assertEqual(max_number(10457), 75410)
+        self.assertEqual(max_number(100000), 100000)
+        self.assertEqual(max_number(33556988), 98865533)
+
+
+if __name__ == '__main__':
+    unittest.main()
